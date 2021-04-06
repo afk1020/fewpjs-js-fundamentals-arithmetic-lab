acba741dff50c8f7ca408e13ef182b20d32d31be
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-04-06 14:21:26 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var num1 = 2;
+var num2 = 31;
+let multiply = (num1 * num2);
 
-// Write your code here
+let random =(Math.floor(Math.random()*2)+1)
+
+var num3 =28;
+var num4 = 8;
+let mod = (num3 % num4);
+
+let max = Math.max(1, 2, 3, 20)
